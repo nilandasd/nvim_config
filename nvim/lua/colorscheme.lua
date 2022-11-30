1229246c40f08f2e50f1a4d8fd7c8c95f328bfda
Niland Schumacher <naschuma@ucsc.edu>
2022-11-30 13:34:28 -0800

    init commit

---
+++ nvim/lua/colorscheme.lua
@@ -0,0 +1,9 @@
+vim.cmd[[set t_Co=256]]
+vim.o.background = "dark"
+local colorscheme = "codedark"
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+  vim.notify("colorscheme " .. colorscheme .. " not found!")
+  return
+end
